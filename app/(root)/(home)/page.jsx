c4ced679768cd67e4b4dbf3b4fd8d3dbac89c95d
Sobitov2002 <abubakrsobitov2002@gmail.com>
2026-01-10 20:11:29 +0500

    Started

---
+++ app/(root)/(home)/page.jsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div className="text-red-500">page</div>;
+};
+
+export default page;
---
+++ app/(root)/layout.jsx
@@ -0,0 +1,13 @@
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <div>
+ 
+      <div>
+        {children}
+      </div>
+    </div>
+  );
+}
